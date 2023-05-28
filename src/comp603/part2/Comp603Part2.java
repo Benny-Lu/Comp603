a825ea2671dae86d8244455594647dbd419c0007
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-05-28 23:14:02 +1200

    GUI Update
    +Added a Welcome message to the GUI interface
    +Added a Book Button for Finding a Book Room
    +Added a View Button for See my Bookings

--- src/comp603/part2/Comp603Part2.java
+++ src/comp603/part2/Comp603Part2.java
@@ -13,23 +13,76 @@ import javax.swing.JFrame;
  *
  * @author GGPC
  */
-public class Comp603Part2 {
+public class Comp603Part2 extends JFrame {
 
-    /**
-     * @param args the command line arguments
-     */
-     public static void main(String args[]){
-       JFrame frame = new JFrame("Hotel Booking System");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(500,500);
-       JButton button = new JButton("Press");
-       frame.getContentPane().add(button); // 
-       frame.setVisible(true);
+    private JLabel outputLabel;
+
+    public Comp603Part2() {
+        setTitle("Hotel Booking Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 600);
+
+        // Create GUI components
+        JButton bookButton = new JButton("Find and Book a Room");
+        JButton viewButton = new JButton("See My Bookings");
+
+        outputLabel = new JLabel("Welcome to the Hotel Booking Application");
+        outputLabel.setHorizontalAlignment(SwingConstants.CENTER); // Align label text in the center
+
+        // Create panels for layout
+        JPanel topPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+
+        // Set layout for the main content pane
+        setLayout(new BorderLayout());
+
+        // Set layout for panels
+        topPanel.setLayout(new FlowLayout());
+        centerPanel.setLayout(new GridBagLayout()); // Use GridBagLayout for precise component placement
+        bottomPanel.setLayout(new FlowLayout());
+
+        // Add components to panels
+        topPanel.add(outputLabel);
+        
+        // Add buttons to the center panel using GridBagLayout
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 0, 10, 0); // Add vertical spacing
+        centerPanel.add(bookButton, gbc);
+
+        gbc.gridy = 1;
+        centerPanel.add(viewButton, gbc);
+
+        bottomPanel.add(Box.createVerticalStrut(50)); // Add spacing between the panels
+
+        // Add panels to the main content pane
+        add(topPanel, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
     }
-     //Confiming Git is working
-    
-     public void Message()
-     {
-         System.out.println("Hello!");
-     }
+
+    private void findAndBookRoom() {
+        // Implement the logic to find and book a room
+        // Update the outputLabel or other GUI components accordingly
+        outputLabel.setText("Find and Book a Room");
+    }
+
+    private void seeMyBookings() {
+        // Implement the logic to see my bookings
+        // Update the outputLabel or other GUI components accordingly
+        outputLabel.setText("See My Bookings");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Comp603Part2();
+            }
+        });
+    }
 }

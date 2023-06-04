35f6f07a5fbfc1c638fee5f1595eb8e7bd984c78
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-06-05 01:07:58 +1200

    +Added a SeeMyBookings page for the button in the main menu

---
+++ build/classes/comp603/part2/SeeMyBookings.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp603.part2;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+/**
+ *
+ * @author GGPC
+ */
+public class SeeMyBookings extends JFrame {
+
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+
+    public SeeMyBookings() {
+        setTitle("Hotel Booking Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 600);
+
+        // Create panel for card layout
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        // Create initial panel
+        JPanel initialPanel = new JPanel();
+        initialPanel.setLayout(new BorderLayout());
+        JLabel titleLabel = new JLabel("Welcome to the Hotel Booking Application");
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        initialPanel.add(titleLabel, BorderLayout.CENTER);
+
+        // Add initial panel to card panel
+        cardPanel.add(initialPanel, "initial");
+
+        // Set card panel as the main content pane
+        setContentPane(cardPanel);
+
+        setVisible(true);
+    }
+
+    public void viewBooking() {
+        // Clear the card panel
+        cardPanel.removeAll();
+
+        // Create panel for "Find and Book a Room" section
+        JPanel findAndBookRoomPanel = new JPanel();
+        findAndBookRoomPanel.setLayout(new BorderLayout());
+
+        // Create a panel for button and title
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        // Create a back button
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Go back to the previous panel
+                cardLayout.show(cardPanel, "initial");
+                Comp603Part2 newframe = new Comp603Part2();
+                newframe.setVisible(true);
+                newframe.toFront();
+                dispose(); // Close the current JFrame
+            }
+        });
+        buttonPanel.add(backButton); // Add the button to the panel
+
+        // Create a label for the title and align it at the top
+        JLabel titleLabel = new JLabel("Here is where you see your bookings");
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setVerticalAlignment(SwingConstants.TOP); // Align the text at the top
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+        // Add the button panel and title label to the findAndBookRoomPanel
+        findAndBookRoomPanel.add(buttonPanel, BorderLayout.NORTH);
+        findAndBookRoomPanel.add(titleLabel, BorderLayout.CENTER);
+
+        // Add the panel to the card panel
+        cardPanel.add(findAndBookRoomPanel, "findAndBookRoom");
+
+        // Show the "Find and Book a Room" panel
+        cardLayout.show(cardPanel, "findAndBookRoom");
+
+        // Refresh the GUI
+        revalidate();
+        repaint();
+    }
+
+}
